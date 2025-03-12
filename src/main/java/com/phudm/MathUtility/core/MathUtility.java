6d28702b7f2018147053a00f91cd113bf82417fc
PhuJs412 <phujason1992@gmail.com>
2025-03-12 11:39:57 +0700

    Update getFactorialMehodToUseRecursion

--- src/main/java/com/phudm/MathUtility/core/MathUtility.java
+++ src/main/java/com/phudm/MathUtility/core/MathUtility.java
@@ -9,23 +9,40 @@ public class MathUtility {
         //quy ước hàm này chỉ tính n từ 0 ... 20
         //code hàm thì cx đồng thời phải kiểm thử hàm; muốn kiểm thử phải có test case!!!
 
-        public static long getFactorial(int n){
-            if(n < 0 || n > 20){
-                //return -1;
-                throw new IllegalArgumentException("n must be between 0 and 20");
-            }
-            if(n == 0){
-                return 1; //Early return, thoát sớm nếu có thể !!!
-            }
-            //Khỏi viết else cho code nhìn đẹp
-            // n bắt đầu từ 1...
+//        public static long getFactorial(int n){
+//            if(n < 0 || n > 20){
+//                //return -1;
+//                throw new IllegalArgumentException("n must be between 0 and 20");
+//            }
+//            if(n == 0){
+//                return 1; //Early return, thoát sớm nếu có thể !!!
+//            }
+//            //Khỏi viết else cho code nhìn đẹp
+//            // n bắt đầu từ 1...
+//
+//            // thuật con heo đất, nhân dồn, cộng dồn
+//            long result = 1;
+//            for (int i = 1; i <= n ; i++) {
+//                    result *= i; //i bị nhân dồn vào result = result * i;
+//            }
+//            return result;
+//    }
 
-            // thuật con heo đất, nhân dồn, cộng dồn
-            long result = 1;
-            for (int i = 1; i <= n ; i++) {
-                    result *= i; //i bị nhân dồn vào result = result * i;
-            }
-            return result;
+//CHUYỂN SANG TÍNH GIAI THỪA BẰNG ĐỆ QUY, GỌI LẠI CHÍNH MÌNH VƠỚI QUY MÔ NHO HƠN
+    //N! = N  * (N-1)!
+    public static long getFactorial(int n){
+        if(n < 0 || n > 20){
+            //return -1;
+            throw new IllegalArgumentException("n must be between 0 and 20");
+        }
+        if(n == 0){
+            return 1; //Early return, thoát sớm nếu có thể !!!
+        }
+        //Khỏi viết else cho code nhìn đẹp
+        // n bắt đầu từ 1...
+
+        // công thức de quy
+        return n * getFactorial(n - 1);
     }
 
     // TEST CASE #2
